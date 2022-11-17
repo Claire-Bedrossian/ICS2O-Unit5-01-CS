2546fe1beb064a8fa377bac4d53c5559f5bac660
Claire Bedrossian <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-11-17 00:40:56 +0000

    added function

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ICS2O-Unit5-01-CS
+
+[![Mr Coxall's Super Linter](https://github.com/Claire-Bedrossian/ICS2O-Unit5-01-CS/workflows/Mr%20Coxall's%20Super%20Linter/badge.svg)](https://github.com/Claire-Bedrossian/ICS2O-Unit5-01-CS/actions)
+
+[![Run on Repl.it](https://repl.it/badge/github/Claire-Bedrossian/ICS2O-Unit5-01-CS)](https://repl.it/github/Claire-Bedrossian/ICS2O-Unit5-01-CS)
---
+++ main.cs
@@ -0,0 +1,34 @@
+// Created by: Claire Bedrossian
+// Created on: Nov 2022
+//
+// This program is a game to guess a random number the computer has generated.
+
+using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        System.Random random = new System.Random();
+        // This function accepts user input
+        float guess;
+        // Returns an `int` value greater in the range 10 <= value < 50
+        float num = random.Next(1,6);
+            
+        Console.WriteLine("Pick a number from one to six on the slider below. You must try and guess the random number the computer has chosen...");
+        Console.WriteLine("");
+
+        Console.Write("Enter your guess: ");
+        guess = Convert.ToSingle(Console.ReadLine());
+
+        if (guess == num){
+            Console.WriteLine("That is the right answer.");
+        }
+
+        if (guess != num){
+            Console.WriteLine("That is the wrong answer.");
+        }
+        Console.WriteLine("");
+        Console.WriteLine("\nDone.");
+    }
+}
